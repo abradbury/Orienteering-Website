b86aaa6cc0a4e3d54dd80cda6020b621d740da9d
abradbury <adam.b92@virginmedia.com>
2015-10-27 20:31:54 +0000

    Corrected file upload for events

--- templates/syo/uploadify/uploadify.php
+++ templates/syo/uploadify/uploadify.php
@@ -6,7 +6,7 @@ Released under the MIT License <http://www.opensource.org/licenses/mit-license.p
 */
 
 // Define a destination
-$preTargetFolder = '/home/j14sout/public_html/beta';
+$preTargetFolder = '/home/j14sout/public_html';
 $targetFolder = '/event'; // Relative to the root
 
 // Paths for localhost testing
@@ -24,43 +24,39 @@ if (!empty($_FILES)) {
 	$tempFile = $_FILES['Filedata']['tmp_name'];
 	$targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
 	$targetFile = rtrim($targetPath,'/') . '/' . $_FILES['Filedata']['name'];
-	
+
 	// If the folder does not exist, create one
-	if(!file_exists($targetPath))
-	{
+	if(!file_exists($targetPath)) {
 		mkdir(str_replace('//','/',$targetPath), 0755, true);
 	}
 	
 	// Validate the file type
 	$fileTypes = array('pdf', 'jpg', 'jpeg', 'txt', 'rtf', 'doc', 'docx', 'xlsx', 'xls', 'htm', 'html'); // File extensions
 	$fileParts = pathinfo($_FILES['Filedata']['name']);
-	
+
 	// If the 'extension' file part is in the fileTypes array
-	if (in_array($fileParts['extension'],$fileTypes)) 
-	{
+	if (in_array($fileParts['extension'],$fileTypes)) {
+
 		$existingVerions = glob(rtrim($targetPath,'/') . '/' . $documentType . '_v[0-9].' . $fileParts['extension']);
 		$newFile =  $targetFolder . '/' . $documentType . '_v1.' . $fileParts['extension'];
 				
 		// If there are no existing versions
-		if(!$existingVerions) {
+		if(empty($existingVerions)) {
 			move_uploaded_file($tempFile,$targetFile);
 			rename($preTargetFolder . $targetFolder . '/' . $_FILES['Filedata']['name'], $preTargetFolder . $newFile);
-		}
-		// If there are existing versions
-		else
-		{
+		} else {
+			// If there are existing versions
 			// Splits into '/home/j14sout/../flyer_v3' and 'pdf'
 			list($name, $ext) = explode('.', end($existingVerions));
 
 			// Splits into '/home/j14sout/../flyer' and '3'
 			list($basename, $index) = explode('_v', $name);
 			
-			$index++; // increment the index
-			$basename = substr($basename,37);
+			// Increment the index
+			$index++; 
 
-			// TODO: Change 37 - hardcoded based specific URL
+			$newFile = $targetFolder . '/' . $documentType . '_v' . $index . '.' . $ext;
 
-			$newFile = $basename . '_v' . $index . '.' . $ext;
 			move_uploaded_file($tempFile,$targetFile);
 			rename($preTargetFolder . $targetFolder . '/' . $_FILES['Filedata']['name'], $preTargetFolder . $newFile);
 		}
