1133195b862d12b22664d3ad5169e559d4f0f51b
abradbury <adam.b92@virginmedia.com>
2015-10-27 21:28:24 +0000

    Added HTML5shiv for IE < 9

--- templates/syo/index.php
+++ templates/syo/index.php
@@ -30,6 +30,9 @@ $doc->addStyleSheet($this->baseurl.'/templates/'.$this->template.'/css/font-awes
 
 // Add JS
 $doc->addScript($this->baseurl.'/templates/'.$this->template.'/js/bootstrap.min.js');
+
+// HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries
+$doc->addCustomTag('<!--[if lt IE 9]><script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script><script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->');
 ?>
 
 <!DOCTYPE html>
