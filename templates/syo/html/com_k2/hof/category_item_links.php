c6f6b1cebcd58e974471523725d18d576845c4e6
Adam Bradbury <adam.b92@virginmedia.com>
2017-11-20 21:12:53 +0000

    Aligned git repo to deployed site

---
+++ templates/syo/html/com_k2/hof/category_item_links.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @version		2.6.x
+ * @package		K2
+ * @author		JoomlaWorks http://www.joomlaworks.net
+ * @copyright	Copyright (c) 2006 - 2014 JoomlaWorks Ltd. All rights reserved.
+ * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+// Define default image size (do not change)
+K2HelperUtilities::setDefaultImage($this->item, 'itemlist', $this->params);
+
+?>
+
+<li>
+	<a href="<?php echo $this->item->link; ?>">
+		<?php echo $this->item->title; ?>
+	</a>
+</li>
